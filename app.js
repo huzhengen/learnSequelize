42f954d09505d25574c5741d207953701a15648a
hukai <huzhengen@163.com>
2019-08-22 17:17:22 +0800

    让Sequelize在执行时不把将sql语句输出到控制台

--- app.js
+++ app.js
@@ -6,17 +6,20 @@ const sequelize = new Sequelize('database', 'username', 'password', {
 
     // the storage engine for sqlite
     // - default ':memory:'
-    storage: 'database/database.sqlite'
+    storage: 'database/database.sqlite',
+
+    // disable logging; default: console.log
+    logging: false,
 })
 
 sequelize
-	.authenticate()
-	.then(() => {
-		console.log('Connection has been established successfully.');
-	})
-	.catch(err => {
-		console.error('Unable to connect to the database:', err);
-	});
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 sequelize.sync(
     // { force: true }
